8a64de92e67d5ece73992f88936c8c0622d10a9c
Maciej Pastuszka <maciej.pastuszka@syzygy.pl>
2021-02-27 13:24:40 +0100

    initial config

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 100, // line width
+  tabWidth: 2,
+  singleQuote: true, // not applied to JSX
+  semi: true, // use semicolons
+  trailingComma: "es5", // use trailing comma where it makes sense in es5
+  jsxBracketSameLine: false
+};
+
--- components/hero-post.js
+++ components/hero-post.js
@@ -1,31 +1,24 @@
-import Avatar from '../components/avatar'
-import Date from '../components/date'
-import CoverImage from '../components/cover-image'
-import Link from 'next/link'
+import classNames from 'classnames/bind';
 
-export default function HeroPost({
-  title,
-  coverImage,
-  date,
-  excerpt,
-  author,
-  slug,
-}) {
+import Avatar from '../components/avatar';
+import Date from '../components/date';
+import CoverImage from '../components/cover-image';
+import Link from 'next/link';
+import styles from './hero-post.module.css';
+
+const cx = classNames.bind(styles);
+
+export default function HeroPost({ title, coverImage, date, excerpt, author, slug }) {
   return (
     <section>
-      <div className="mb-8 md:mb-16">
-        {coverImage && (
-          <CoverImage title={title} coverImage={coverImage} slug={slug} />
-        )}
+      <div className={cx('hero')}>
+        {coverImage && <CoverImage title={title} coverImage={coverImage} slug={slug} />}
       </div>
       <div className="md:grid md:grid-cols-2 md:col-gap-16 lg:col-gap-8 mb-20 md:mb-28">
         <div>
           <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
             <Link as={`/posts/${slug}`} href="/posts/[slug]">
-              <a
-                className="hover:underline"
-                dangerouslySetInnerHTML={{ __html: title }}
-              />
+              <a className="hover:underline" dangerouslySetInnerHTML={{ __html: title }} />
             </Link>
           </h3>
           <div className="mb-4 md:mb-0 text-lg">
@@ -41,5 +34,5 @@ export default function HeroPost({
         </div>
       </div>
     </section>
-  )
+  );
 }
